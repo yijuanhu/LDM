7f1f2b271d58514e0ad2fa9051e302233816c5cc
Yijuan Hu <yijuan.hu@emory.edu>
2023-09-07 04:12:56 -0400

    Add files via upload

--- tests/testthat/test-permanovaFL.R
+++ tests/testthat/test-permanovaFL.R
@@ -8,7 +8,7 @@ data(throat.meta)
 # test
 test_that("`permanovaFL` function provides expected results", {
     res.perm <- permanovaFL(throat.otu.tab5 | (Sex+AntibioticUse) ~ SmokingStatus+PackYears, 
-                            data=throat.meta, dist.method="bray", seed=82955, n.cores=1)
+                            data=throat.meta, dist.method="bray", seed=82955, n.cores=1, verbose=FALSE)
     res_p <- signif(res.perm$p.permanova, 3)
     expect_equivalent(res_p, c(0.0022, 0.6450))
 })
